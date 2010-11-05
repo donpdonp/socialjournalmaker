a390d8b44e8c4317389110af038287fa5308cff6
Don Park <donp@everyonedelivers.com>
2010-11-05 11:43:00 -0700

    clean rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sjmaker::Application.config.secret_token = '823a0a686e30ff5a56dfa287bb77ba3436e56ad7e7e4b786f3971fb909a8ff351cd48efbda46e85371a91043faf55f0ed997e8eb6fd2e5476a0080fc3527e49d'
